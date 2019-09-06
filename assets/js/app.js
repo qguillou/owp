2b90c7371da6787feea8b0aa0f41e62ce81bddd0
qguillou <guillou.quentin.29@gmail.com>
2019-09-06 20:59:48 +0200

    Configure project style with Bootstrap

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+// app.js
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// any CSS you require will output into a single css file (app.css in this case)
+require('../css/global.scss');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});

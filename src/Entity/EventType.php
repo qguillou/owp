abbd8021818c020bb7c7fb18c19a951d955a554d
Quentin GUILLOU <quentin.guillou@atos.net>
2019-09-10 11:28:58 +0200

    Adding Event Type entity

---
+++ src/Entity/EventType.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\User;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EventTypeRepository")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class EventType
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $label;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(nullable=true)
+     */
+    private $createBy;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(nullable=true)
+     */
+    private $updateBy;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updateAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): self
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getCreateBy(): ?User
+    {
+        return $this->createBy;
+    }
+
+    public function setCreateBy(User $user): self
+    {
+        $this->createBy = $user;
+
+        return $this;
+    }
+
+    public function getCreateAt(): ?\DateTimeInterface
+    {
+        return $this->createAt;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function setCreateAt(): self
+    {
+        $this->createAt = new \DateTime();
+
+        return $this;
+    }
+
+    public function getUpdateAt(): ?\DateTimeInterface
+    {
+        return $this->updateAt;
+    }
+
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    public function setUpdateAt(): self
+    {
+        $this->updateAt = new \DateTime();
+
+        return $this;
+    }
+
+    public function getUpdateBy(): ?User
+    {
+        return $this->updateBy;
+    }
+
+    public function setUpdateBy(user $user): self
+    {
+        $this->updateBy = $user;
+
+        return $this;
+    }
+}

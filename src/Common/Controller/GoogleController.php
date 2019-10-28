41039db475a69b864147db8087f5eadc30bd9505
Quentin GUILLOU <quentin.guillou@atos.net>
2019-10-28 11:13:14 +0100

    Adding google authentification

---
+++ src/Common/Controller/GoogleController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Common\Controller;
+
+
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GoogleController extends AbstractController
+{
+    /**
+     * Link to this controller to start the "connect" process
+     *
+     * @param ClientRegistry $clientRegistry
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function loginAction(ClientRegistry $clientRegistry)
+    {
+        return $clientRegistry
+            ->getClient('google')
+            ->redirect();
+    }
+
+    /**
+     * Google redirects to back here afterwards
+     *
+     * @param Request $request
+     * @return JsonResponse|\Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function loginCheckAction(Request $request)
+    {
+        if (!$this->getUser()) {
+            return new JsonResponse(array('status' => false, 'message' => "User not found!"));
+        } else {
+            return $this->redirectToRoute('default');
+        }
+
+    }
+
+}

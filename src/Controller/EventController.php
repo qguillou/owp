4c0bf2e0ca4dd4ca0237b5c06d95b94af14dd877
Quentin GUILLOU <quentin.guillou@atos.net>
2019-11-06 18:16:24 +0100

    Entity - Event - Adding field and beginning template

---
+++ src/Controller/EventController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\EventRepository;
+
+class EventController extends AbstractController
+{
+    /**
+     * @Route("/event/{id}", name="owp_event_show")
+     */
+    public function show($id, EventRepository $eventRepository): Response
+    {
+        return $this->render('Event/show.html.twig', [
+            'event' => $eventRepository->find($id),
+        ]);
+    }
+
+}

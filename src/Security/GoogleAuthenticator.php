148a00a491206fe0044872a0ecc5f305f6a19047
qguillou <guillou.quentin.29@gmail.com>
2019-11-11 17:17:52 +0100

    Fixing google connection

--- src/Security/GoogleAuthenticator.php
+++ src/Security/GoogleAuthenticator.php
@@ -14,25 +14,144 @@ use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 class GoogleAuthenticator extends SocialAuthenticator
 {
+    /**
+     * @var ClientRegistry
+     */
     private $clientRegistry;
-    private $em;
-    private $router;
 
-    public function __construct(ClientRegistry $clientRegistry, EntityManagerInterface $em, RouterInterface $router)
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * @var UserManagerInterface
+     */
+    private $userManager;
+
+    /**
+     * GoogleAuthenticator constructor.
+     * @param ClientRegistry $clientRegistry
+     * @param EntityManagerInterface $em
+     * @param UserManagerInterface $userManager
+     */
+    public function __construct(ClientRegistry $clientRegistry, EntityManagerInterface $em, UserManagerInterface $userManager)
     {
         $this->clientRegistry = $clientRegistry;
         $this->em = $em;
-        $this->router = $router;
+        $this->userManager = $userManager;
     }
 
+    /**
+     * @param Request $request
+     * @return bool
+     */
     public function supports(Request $request)
     {
-        return $request->getPathInfo() == '/connect/google/check' && $request->isMethod('GET');
+        // continue ONLY if the current ROUTE matches the check ROUTE
+        return $request->attributes->get('_route') === 'connect_google_check';
     }
 
+    /**
+     * @param Request $request
+     * @return \League\OAuth2\Client\Token\AccessToken|mixed
+     */
     public function getCredentials(Request $request)
     {
+        // this method is only called if supports() returns true
+
         return $this->fetchAccessToken($this->getGoogleClient());
+    }
+
+    /**
+     * @param mixed $credentials
+     * @param UserProviderInterface $userProvider
+     * @return User|null|object|\Symfony\Component\Security\Core\User\UserInterface
+     */
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        /** @var GoogleUser $googleUser */
+        $googleUser = $this->getGoogleClient()
+            ->fetchUserFromToken($credentials);
+
+        $email = $googleUser->getEmail();
+
+        // 1) have they logged in with Google before? Easy!
+        $existingUser = $this->em->getRepository(User::class)
+            ->findOneBy(['googleId' => $googleUser->getId()]);
+
+        if ($existingUser) {
+            $user = $existingUser;
+        } else {
+            // 2) do we have a matching user by email?
+            $user = $this->em->getRepository(User::class)
+                ->findOneBy(['email' => $email]);
+
+            if (!$user) {
+                /** @var User $user */
+                $user = $this->userManager->createUser();
+                $user->setEnabled(true);
+                $user->setEmail($email);
+                $user->setUsername("your chosen username");
+                $user->setPlainPassword("your chosen password");
+            }
+        }
+
+        // 3) Maybe you just want to "register" them by creating
+        // a User object
+        $user->setGoogleId($googleUser->getId());
+        $this->userManager->updateUser($user);
+
+        return $userProvider->loadUserByUsername($user->getUsername());
+    }
+
+    /**
+     * @return GoogleClient
+     */
+    private function getGoogleClient()
+    {
+        return $this->clientRegistry->getClient('google');
+    }
+
+    /**
+     * @param Request $request
+     * @param TokenInterface $token
+     * @param string $providerKey
+     * @return null|Response
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        // on success, let the request continue
+        return null;
+    }
+
+    /**
+     * @param Request $request
+     * @param AuthenticationException $exception
+     * @return null|Response
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        $message = strtr($exception->getMessageKey(), $exception->getMessageData());
+
+        return new Response($message, Response::HTTP_FORBIDDEN);
+    }
+
+    /**
+     * Called when authentication is needed, but it's not sent.
+     * This redirects to the 'login'.
+     *
+     * @param Request $request
+     * @param AuthenticationException|null $authException
+     *
+     * @return RedirectResponse
+     */
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        return new RedirectResponse(
+            '/connect/', // might be the site, where users choose their oauth provider
+            Response::HTTP_TEMPORARY_REDIRECT
+        );
     }
 
     public function getUser($credentials, UserProviderInterface $userProvider)
@@ -55,74 +174,5 @@ class GoogleAuthenticator extends SocialAuthenticator
         }
 
         return $user;
-    }
-
-    /**
-     * @return \KnpU\OAuth2ClientBundle\Client\OAuth2Client
-     */
-    private function getGoogleClient()
-    {
-        return $this->clientRegistry
-            ->getClient('google');
-    }
-
-    /**
-     * Returns a response that directs the user to authenticate.
-     *
-     * This is called when an anonymous request accesses a resource that
-     * requires authentication. The job of this method is to return some
-     * response that "helps" the user start into the authentication process.
-     *
-     * Examples:
-     *  A) For a form login, you might redirect to the login page
-     *      return new RedirectResponse('/login');
-     *  B) For an API token authentication system, you return a 401 response
-     *      return new Response('Auth header required', 401);
-     *
-     * @param Request $request The request that resulted in an AuthenticationException
-     * @param \Symfony\Component\Security\Core\Exception\AuthenticationException $authException The exception that started the authentication process
-     *
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function start(Request $request, \Symfony\Component\Security\Core\Exception\AuthenticationException $authException = null)
-    {
-        return new RedirectResponse('/login');
-    }
-
-    /**
-     * Called when authentication executed, but failed (e.g. wrong username password).
-     *
-     * This should return the Response sent back to the user, like a
-     * RedirectResponse to the login page or a 403 response.
-     *
-     * If you return null, the request will continue, but the user will
-     * not be authenticated. This is probably not what you want to do.
-     *
-     * @param Request $request
-     * @param \Symfony\Component\Security\Core\Exception\AuthenticationException $exception
-     *
-     * @return \Symfony\Component\HttpFoundation\Response|null
-     */
-    public function onAuthenticationFailure(Request $request, \Symfony\Component\Security\Core\Exception\AuthenticationException $exception)
-    {
-    }
-
-    /**
-     * Called when authentication executed and was successful!
-     *
-     * This should return the Response sent back to the user, like a
-     * RedirectResponse to the last page they visited.
-     *
-     * If you return null, the current request will continue, and the user
-     * will be authenticated. This makes sense, for example, with an API.
-     *
-     * @param Request $request
-     * @param \Symfony\Component\Security\Core\Authentication\Token\TokenInterface $token
-     * @param string $providerKey The provider (i.e. firewall) key
-     *
-     * @return void
-     */
-    public function onAuthenticationSuccess(Request $request, \Symfony\Component\Security\Core\Authentication\Token\TokenInterface $token, $providerKey)
-    {
     }
 }

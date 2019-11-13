c1ae2c02a06c4cbcc95121bcaf416da12366f2f3
qguillou <guillou.quentin.29@gmail.com>
2019-11-13 22:54:12 +0100

    Update date format display

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -20,11 +20,11 @@ class AppExtension extends AbstractExtension
         setlocale(LC_TIME, 'fr_FR.UTF8', 'fr.UTF8', 'fr_FR.UTF-8', 'fr.UTF-8');
 
         if (empty($end) || $start->getTimestamp() === $end->getTimestamp()) {
-            $date = strftime('%A %d %B %Y %H:%M', $start->getTimestamp());
+            $date = strftime('%A %d %B %Y à %H:%M', $start->getTimestamp());
         } elseif ($start->format('d') === $end->format('d')) {
-            $date = strftime('%A %d %B %Y %H:%M', $start->getTimestamp()) . ' - ' . strftime('%H:%M', $start->getTimestamp());
+            $date = strftime('%A %d %B %Y de %H:%M', $start->getTimestamp()) . ' - ' . strftime(' à %H:%M', $start->getTimestamp());
         } else {
-            $date = strftime('%A %d %B %Y %H:%M', $start->getTimestamp()) . ' - ' . strftime('%A %d %B %Y %H:%M', $end->getTimestamp());
+            $date = strftime('\D\u %A %d %B %Y à %H:%M \a\u ', $start->getTimestamp()) . ' - ' . strftime('%A %d %B %Y à %H:%M', $end->getTimestamp());
         }
 
         return $date;
